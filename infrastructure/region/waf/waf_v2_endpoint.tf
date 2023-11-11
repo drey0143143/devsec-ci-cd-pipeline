1273cd2b4b00e5655dd05a2114a8125c41c0de55
oodo_ptcinc <oodo@ptc.com>
2023-11-11 17:47:25 -0600

    more changes

--- infrastructure/region/waf/waf_v2_endpoint.tf
+++ infrastructure/region/waf/waf_v2_endpoint.tf
@@ -10,7 +10,7 @@ module "wafv2" {
   environment               = "DEV"
   functionality             = "WAFv2"
   enabled                   = false
-  name_prefix               = "reinvent"
+  name_prefix               = "reinvent-aws"
   description               = "WAFv2 to protect internet facing endpoints"
 
   visibility_config = {
